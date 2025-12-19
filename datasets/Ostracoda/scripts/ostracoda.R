ccd2a7909466db7265c7436a63b2e7f6cd9bdc2e
Louis Duchemin <louis.duchemin@univ-lyon1.fr>
2025-12-19 11:54:03 +0100

    feat: issues processing for Ostracoda dataset

---
+++ datasets/Ostracoda/scripts/ostracoda.R
@@ -0,0 +1,138 @@
+library(tidyverse)
+
+(
+  taxonomy_fix = read_tsv("datasets/Ostracoda/data/taxonomy_fix.tsv") %>%
+    mutate(
+      unclassified = is.na(Classified) | !Classified,
+      id_confer = str_detect(Lineage, "cf\\."),
+      taxonRank = str_to_title(taxonRank)
+    ) %>%
+    select(ID, family, genus, species, scientificNameAuthorship, unclassified, taxonRank, Lineage, id_confer, scientificName)
+)
+
+(
+  aff_cf_fix = read_tsv("datasets/Ostracoda/data/aff_cf.tsv") %>%
+    mutate(
+      id_confer = str_detect(Lineage, "cf\\."),
+    ) %>%
+    select(ID, family, genus, species, scientificNameAuthorship, taxonRank, Lineage, id_confer, scientificName)
+)
+
+
+
+
+
+(
+  # Dataset.tsv is manually fixed version of Dataset.csv where line break in record 1469 was removed.
+  data <- read_tsv("datasets/Ostracoda/data/Dataset.tsv") %>%
+    select(-geodeticDatum, -TaxonID) %>%
+    mutate(
+      id_confer = str_detect(verbatimIdentification, "cf"),
+    ) %>%
+    left_join(taxonomy_fix, by = "ID", suffix = c(".old", ".new")) %>%
+    mutate(
+      family = coalesce(family.new, family.old),
+      genus = coalesce(genus.new, genus.old),
+      species = coalesce(species.new, species.old),
+      scientificNameAuthorship = coalesce(scientificNameAuthorship.new, scientificNameAuthorship.old),
+      scientificName = coalesce(scientificName.new, scientificName.old),
+      taxonRank = coalesce(taxonRank.new, taxonRank.old),
+      Lineage = coalesce(Lineage.new, Lineage.old),
+      id_confer = coalesce(id_confer.new, id_confer.old),
+    ) %>%
+    # drop .old columns
+    select_at(vars(-ends_with(".old"), -ends_with(".new"))) %>%
+    left_join(aff_cf_fix, by = "ID", suffix = c(".old", ".new")) %>%
+    mutate(
+      family = coalesce(family.new, family.old),
+      genus = coalesce(genus.new, genus.old),
+      species = coalesce(species.new, species.old),
+      scientificNameAuthorship = coalesce(scientificNameAuthorship.new, scientificNameAuthorship.old),
+      scientificName = coalesce(scientificName.new, scientificName.old),
+      taxonRank = coalesce(taxonRank.new, taxonRank.old),
+      Lineage = coalesce(Lineage.new, Lineage.old),
+      id_confer = coalesce(id_confer.new, id_confer.old),
+    ) %>%
+    # drop .old and .new columns
+    select_at(vars(-ends_with(".old"), -ends_with(".new"))) %>%
+    select(-verticalDatum, -locationID, -`Input.into.DB.-.name`, -rightsHolder) %>%
+    mutate(
+      references = if_else(
+        ID %in% seq(706, 717),
+        "Issartel, C., & Marmonier, P. (2025). Description of five new species of Schellencandona Meisch, 1996 (Ostracoda: Candoninae) from the southern French Alps, a highly diversified area for groundwater ostracods. European Journal of Taxonomy, 1022(1), 85–133. https://doi.org/10.5852/ejt.2025.1022.3083",
+        references
+      ),
+      taxonRank = str_to_title(taxonRank),
+      # After checking, only Typhlocypris cf. eremita is concerned with confer id
+      scientificNameAuthorship = scientificNameAuthorship %>% stringi::stri_trans_general("Latin-ASCII") %>% str_remove_all("\\,"),
+      taxon_name = scientificName %>%
+        stringi::stri_trans_general("Latin-ASCII") %>%
+        str_remove_all("\\,") %>%
+        str_remove_all(scientificNameAuthorship) %>%
+        str_remove_all("\\(\\)") %>%
+        str_remove_all("\\(?([A-Z][a-z]+([A-Z][a-z]+\\,)* ?)*([A-Z][a-z]+ ?\\& ?)* ?[A-Z][a-z]+ [0-9]{4}\\)?") %>%
+        str_remove("((ge)?n\\. *)?sp\\.? *$") %>%
+        str_squish(),
+      # taxon_name = if_else(id_confer, "Typhlocypris eremita", taxon_name) %>% str_remove("Namiotko et al. 2005$"),
+      # species = if_else(id_confer, "eremita", species),
+      unclassified = if_else(unclassified, unclassified, str_detect(verbatimIdentification, "gen\\.|sp\\. ?[^$ ]|aff\\.")),
+    ) %>%
+    bind_rows(
+      .,
+      filter(., ID == 706) %>%
+        mutate(
+          species = "danielopoli",
+          scientificName = "Schellencandona danielopoli Issartel & Marmonier, 2025",
+          taxon_name = "Schellencandona danielopoli",
+          scientificNameAuthorship = "Issartel & Marmonier, 2025",
+        ),
+      filter(., ID == 676) %>%
+        mutate(
+          species = "n. sp. J4",
+          unclassified = TRUE,
+          scientificName = "Schellencandona sp. J4, 2006",
+          verbatimIdentification = "Schellencandona sp. J4, 2006",
+          taxon_name = "Schellencandona n. sp. J4",
+          scientificNameAuthorship = NA,
+        )
+    ) %>%
+    filter(!str_detect(family, "CC0")) %>%
+    mutate(
+      references = str_replace_all(references, "", "è") %>%
+        str_replace_all("eè", "è") %>%
+        str_replace_all("franais", "français") %>%
+        str_replace_all("dâun", "d'un") %>%
+        str_replace_all("karstiqueÊ", "karstique ") %>%
+        str_replace_all("Universityy", "University ") %>%
+        str_replace_all("\\\"\"E. Boegan\\\"\"", "\"E. Boegan\"") %>%
+        str_replace("^Unpubl. data P. ", "Unpublished Data: P. "),
+      data_repository = case_when(
+        str_starts(basisOfRecord, "Pascalis") ~ "PASCALIS",
+        str_starts(basisOfRecord, "ATBI") ~ basisOfRecord,
+        str_starts(basisOfRecord, "NODE") ~ "NODE (Nonmarine Ostracod Distribution in Europe), David J. Horne (ed.)",
+        str_starts(basisOfRecord, "SubBio") ~ "SubBioDB",
+      )
+    ) %>%
+    relocate(ID, verbatimIdentification, scientificName, taxon_name, class, order, family, genus, species, everything())
+) %>%
+  write_tsv("datasets/Ostracoda/res/preprocessed.tsv")
+
+
+data %>%
+  filter(ID == 676) %>%
+  select(0:10)
+
+raw_data <- read_delim("datasets/Ostracoda/data/Dataset.csv", delim = ";")
+
+raw_data %>%
+  filter(scientificName != verbatimIdentification)
+distinct() %>%
+  nrow()
+
+filter(ID == 1145) %>%
+  print(n = 100)
+filter(str_detect(decimalLatitude, "CC0") | is.na(decimalLatitude))
+
+select(taxon_name, unclassified, id_confer) %>%
+  distinct() %>%
+  print(n = 200)
